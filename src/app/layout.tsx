542d5a81492433af4f932866126b0bac1775633b
Joel Thomas <joel@codewithjoe.in>
2025-05-23 10:16:24 +0530

    changed the default theme to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ export default function RootLayout({
     <ProgressBarProvider>
     <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
