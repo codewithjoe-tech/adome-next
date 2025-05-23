4d05c57582ea9f30f7553f378084afb2aeaa0ea7
Joel Thomas <joel@codewithjoe.in>
2025-05-23 22:34:09 +0530

    fix : tenant details not fetching

--- src/providers/tenant-provider/index.tsx
+++ src/providers/tenant-provider/index.tsx
@@ -32,6 +32,7 @@ const TenantProvider = ({ children }: { children: React.ReactNode }) => {
   });
 
   useEffect(() => {
+    
     if (tenantData) {
       console.log(tenantData)
       dispatch(setAppInfo({ tenant : tenantData, schemaName:tenantData.subdomain }));
